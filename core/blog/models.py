347d6644f95f56f010eb3d202de7e8ee8c6fff21
hamid <hamidrezaowji@gmail.com>
2025-01-07 16:03:18 +0100

    correcting circular importance for User

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from accounts.models import User
-# User = get_user_model()
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Post(models.Model):
     """
