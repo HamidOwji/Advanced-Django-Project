e5ba11bef02711947d452936791e3a386a054f85
hamid <hamidrezaowji@gmail.com>
2025-02-03 12:34:26 +0100

    adding put and delete view

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -20,10 +20,18 @@ def postList(request):
         return Response(serializer.data)
         
 
-@api_view()
+@api_view(["GET","PUT", "DELETE"])
 def postDetail(request,id):  
-    post = get_object_or_404(Post,pk=id,status=True)    
-    serializer = PostSerializer(post)    
-    return Response(serializer.data)
-
-       
+    post = get_object_or_404(Post,pk=id,status=True)
+    if request.method == "GET":
+        serializer = PostSerializer(post)    
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(post,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        post.delete()
+        return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)
+    
