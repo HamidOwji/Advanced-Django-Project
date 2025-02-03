a8231221e0ba683abc02b121149a90de19b4a7f5
hamid <hamidrezaowji@gmail.com>
2025-02-03 13:27:16 +0100

    switch to generic view

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly,IsAdminUser
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
 
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
-"""@api_view(["GET","POST"])
+# Example for Function Based View
+"""
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+@api_view(["GET","POST"])
 @permission_classes([IsAuthenticatedOrReadOnly])
 def postList(request):
     if request.method == "GET":
@@ -20,8 +24,28 @@ def postList(request):
         serializer = PostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)"""
+        return Response(serializer.data)
 
+@api_view(["GET","PUT","DELETE"])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def postDetail(request,id):  
+    post = get_object_or_404(Post,pk=id,status=True)
+    if request.method == "GET":
+        serializer = PostSerializer(post)    
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(post,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        post.delete()
+        return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)
+"""
+        
+# Example for ApiView in Class Based View 
+'''
+from rest_framework.views import APIView
 class PostList(APIView):
     """getting a list of posts and creating new posts"""
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -39,25 +63,6 @@ class PostList(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-
-
-# @api_view(["GET","PUT", "DELETE"])
-# @permission_classes([IsAuthenticatedOrReadOnly])
-# def postDetail(request,id):  
-#     post = get_object_or_404(Post,pk=id,status=True)
-#     if request.method == "GET":
-#         serializer = PostSerializer(post)    
-#         return Response(serializer.data)
-#     elif request.method == "PUT":
-#         serializer = PostSerializer(post,data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data)
-#     elif request.method == "DELETE":
-#         post.delete()
-#         return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)
-    
-
 
 class PostDetail(APIView):
     """ getting detail of the post and edit plus removing it """
@@ -83,4 +88,20 @@ class PostDetail(APIView):
         post = get_object_or_404(Post,pk=id,status=True)
         post.delete()
         return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)
+'''
+
+# Example for GenericApiView in Class Based View
+class PostList(ListCreateAPIView):
+    """getting a list of posts and creating new posts"""
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
+
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    """ getting detail of the post and edit plus removing it """
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)    
+
 
