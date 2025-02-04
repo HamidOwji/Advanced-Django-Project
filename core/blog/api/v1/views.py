9716c22dff5662a7874c8a8ee7fc161efee034ac
hamid <hamidrezaowji@gmail.com>
2025-02-04 14:39:41 +0100

    Adding object permissions

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import action
+from .permissions import IsOwnerOrReadOnly
 
 # Example for Function Based View
 """
