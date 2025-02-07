fbc960b8100c33bbc49d0e5e9efda67767199544
hamid <hamidrezaowji@gmail.com>
2025-02-07 14:53:40 +0100

    account activation and resend done

--- core/accounts/api/v1/urls/accounts.py
+++ core/accounts/api/v1/urls/accounts.py
@@ -11,9 +11,11 @@ urlpatterns = [
     path('registration/',views.RegistrationApiView.as_view(),name='registration'),
     path('test-email',views.TestEmailSend.as_view(),name='test-email'),
     # activation
-    #path('activation/confirm/')
+    path('activation/confirm/<str:token>',views.ActivationApiView.as_view(),name='activation'),
     # resend activation
-    #path('activation/resend/')
+    path('activation/resend/',views.ActivationResendApiView.as_view(),name='activation-resend'),
+    
+    
 
 
     # change password
